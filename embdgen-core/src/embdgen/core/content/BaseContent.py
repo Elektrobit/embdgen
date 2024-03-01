b11b60124a80ebd8aac5d518896579a106611652
Bo Maryniuk <bo@maryniuk.net>
2024-03-01 16:03:54 +0100

    Fix docstrings

--- embdgen-core/src/embdgen/core/content/BaseContent.py
+++ embdgen-core/src/embdgen/core/content/BaseContent.py
@@ -4,18 +4,23 @@ import abc
 
 from ..utils.SizeType import SizeType
 
+
 class BaseContent(abc.ABC):
-    """Base class for content (i.e. region content)
+    """
+    Base class for content (i.e. region content)
     """
 
     size: SizeType
 
     def __init__(self):
-        "Create a new empty content"
+        """
+        Create a new empty content
+        """
         self.size = SizeType()
 
     def prepare(self) -> None:
-        """Prepare content
+        """
+        Prepare content
 
         This should calculate the size of the content
         and generate any files / information required by
