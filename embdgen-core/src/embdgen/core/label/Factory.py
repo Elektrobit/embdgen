acf3c4f9976f1951084a8e3ece974317101d869d
Bo Maryniuk <bo@maryniuk.net>
2024-03-01 16:01:18 +0100

    Use absolute imports

--- embdgen-core/src/embdgen/core/label/Factory.py
+++ embdgen-core/src/embdgen/core/label/Factory.py
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 from embdgen.plugins import label
+from embdgen.core.utils.class_factory import FactoryBase
+from embdgen.core.label.BaseLabel import BaseLabel
 
-from ..utils.class_factory import FactoryBase
-from . import BaseLabel
 
 class Factory(FactoryBase[BaseLabel]):
     """
