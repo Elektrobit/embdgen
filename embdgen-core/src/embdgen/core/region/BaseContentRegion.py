24fea8dcf33bec4ad0c4d3cdb80266c615ad5df5
Bo Maryniuk <bo@maryniuk.net>
2024-03-04 15:00:44 +0100

    Annotate type of attribute "size", which is unable to be retrieved

--- embdgen-core/src/embdgen/core/region/BaseContentRegion.py
+++ embdgen-core/src/embdgen/core/region/BaseContentRegion.py
@@ -1,15 +1,15 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
-import abc
-
-from embdgen.core.region import BaseRegion
-from embdgen.core.content import BaseContent
-from embdgen.core.utils.class_factory import Config
+from embdgen.core.region import BaseRegion  # type: ignore
+from embdgen.core.utils.SizeType import SizeType  # type: ignore
+from embdgen.core.content import BaseContent  # type: ignore
+from embdgen.core.utils.class_factory import Config  # type: ignore
 
 
 @Config("content")
 class BaseContentRegion(BaseRegion):
     content: BaseContent = None
+    size: SizeType
 
     def prepare(self) -> None:
         if not self.size.is_undefined:
