0efb9ac37fd1326900076830e351bd1b5585de98
Bo Maryniuk <bo@maryniuk.net>
2024-03-14 16:45:07 +0000

    Use build location singleton

--- embdgen-core/src/embdgen/core/utils/image.py
+++ embdgen-core/src/embdgen/core/utils/image.py
@@ -3,12 +3,43 @@
 """
 Utility functions for working with images
 """
+from __future__ import annotations
+
 import io
 import os
+import tempfile
+import shutil
+
 from pathlib import Path
 from typing import Optional
-import tempfile
 from fallocate import fallocate, FALLOC_FL_PUNCH_HOLE, FALLOC_FL_KEEP_SIZE # type: ignore
+
+
+class BuildLocation:
+    """
+    Temporary location of the builds
+    """
+    __instance: BuildLocation | None = None
+    path: Path | None = None
+
+    def __new__(cls) -> BuildLocation:
+        if cls.__instance is None:
+            cls.__instance: BuildLocation = super(BuildLocation, cls).__new__(cls)(
+                Path(tempfile.mkdtemp(prefix="embdgen-"))
+            )
+        return cls.__instance
+
+    def __call__(self, p: Path | None) -> BuildLocation:
+        self.remove()
+        if p is not None:
+            self.path = p
+            self.path.mkdir(parents=True, exist_ok=True)
+        return self
+
+    def remove(self) -> None:
+        if self.path is not None and os.path.exists(self.path):
+            shutil.rmtree(self.path)
+
 
 def create_empty_image(filename: Path, size: int) -> None:
     """
@@ -35,7 +66,6 @@ def copy_sparse(out_file: io.BufferedIOBase, in_file: io.BufferedIOBase, size: O
         size = max_size
     elif size > max_size:
         raise Exception(f"Trying to copy {size} B, but the file has only {max_size} B left")
-
 
     zero_block = b"\0" * 4096
     def is_zero(block):
@@ -73,8 +103,8 @@ def copy_sparse(out_file: io.BufferedIOBase, in_file: io.BufferedIOBase, size: O
 
 
 def get_temp_path() -> Path:
-    return Path(get_temp_path.TEMP_PATH) # type: ignore[attr-defined]
-get_temp_path.TEMP_PATH = "tmp" # type: ignore[attr-defined]
+    return BuildLocation().path
+
 
 def get_temp_file(ext: str="") -> Path:
     return Path(tempfile.mktemp(dir=get_temp_path(), suffix=ext))
