4d0324d23c2881d01a58b4c64ee94ddcee89d2d8
Bo Maryniuk <bo@maryniuk.net>
2024-03-18 11:33:08 +0000

    Implement configuration load from string data

--- embdgen-config-yaml/src/embdgen/plugins/config/YAML.py
+++ embdgen-config-yaml/src/embdgen/plugins/config/YAML.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 from pathlib import Path
+from embdgen.core.label import BaseLabel
 import strictyaml as y
 
 from embdgen.core.config.BaseConfig import BaseConfig
@@ -15,6 +16,20 @@ __version__ = "0.0.1"
 class YAML(BaseConfig):
     CONFIG_TYPE = "yaml"
 
+    def _get_schema(self) -> y.OrValidator:
+        return y.OrValidator(
+            LabelValidator(),
+            y.Map(
+                {
+                    y.Optional("contents"): y.Seq(ContentGeneratorValidator()),
+                    "image": LabelValidator(),
+                }
+            ),
+        )
+
+    def _get_label(self, cfg) -> BaseLabel:
+        return cfg["image"].value if cfg.is_mapping() and "image" in cfg else cfg.value
+
     @classmethod
     def probe(cls, filename: Path) -> bool:
         try:
@@ -25,21 +40,10 @@ class YAML(BaseConfig):
             return False
 
     def load(self, filename: Path) -> BaseLabel:
-        root_schema = y.OrValidator(
-            LabelValidator(),
-            y.Map({
-                y.Optional('contents'): y.Seq(ContentGeneratorValidator()),
-                'image': LabelValidator()
-            })
-        )
-
         ContentRegistry.instance().clear()
         with filename.open(encoding="utf-8") as f:
-            conf = y.load(f.read(), root_schema)
+            return self._get_label(y.load(f.read(), self._get_schema()))
 
-        if conf.is_mapping() and "image" in conf:
-            image = conf["image"].value
-        else:
-            image = conf.value
-
-        return image
+    def load_str(self, data: str) -> BaseLabel:
+        ContentRegistry.instance().clear()
+        return self._get_label(y.load(data, self._get_schema()))
